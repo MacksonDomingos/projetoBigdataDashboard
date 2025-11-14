08022e326349bbe35fcdd517820dfeba8d191398
MacksonDomingos <macksonedinis@gmail.com>
2025-11-14 09:56:55 -0300

    Delete run.bat

--- Dashboard Final/run.bat
+++
@@ -1,8 +0,0 @@
-rem Ativa o venv "BigData", para evitar erros
-cd BigData\Scripts
-call activate.bat
-cd ..
-cd ..
-rem Inicia o dashboard por meio do streamlit
-streamlit run dashboard_trabalho.py
-exit
